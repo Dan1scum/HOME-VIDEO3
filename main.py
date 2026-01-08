a276fe926182ea06d6cc451091489b087d15cd9a
dan1scum <belizer808@gmail.com>
2026-01-08 16:49:49 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = 'Empire of the sun'
+    page.theme_mode = ft.ThemeMode.LIGHT
+    text_hello = ft.Text(value='hello world') 
+    # color=ft.Colors.RED_900
+    # text_hello.value = 'Hello'
+    # text_hello.color = ft.Colors.GREEN_900
+    def text_name(_):
+        # print(name_input.label)
+        name = name_input.value.strip()
+
+        if name:
+            text_hello.color = None
+            text_hello.value = f'Hello {name}'
+            name_input.value = None
+        else:
+            text_hello.value = 'Введите имя!'
+            text_hello.color = ft.Colors.RED
+
+    def switch_icon(_):
+        if page.theme_mode == ft.ThemeMode.DARK:
+            page.theme_mode = ft.ThemeMode.LIGHT
+        else:
+            page.theme_mode = ft.ThemeMode.DARK
+
+
+    # text_button = ft.TextButton('SEND')
+    elevated_button = ft.ElevatedButton('send', on_click=text_name, icon=ft.Icons.SEND)
+    # icon_button = ft.IconButton(icon=ft.Icons.SEND)
+
+    name_input = ft.TextField(label='Введиет текст', on_submit=text_name)
+
+    thememode_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_7, on_click=switch_icon)
+
+    page.add(text_hello, name_input, elevated_button, thememode_button)
+
+
+
+ft.app(target=main)
